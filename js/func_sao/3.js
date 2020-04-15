89b555007376721ce216a242621d365f9eec956a
lqt0327 <56208949+lqt0327@users.noreply.github.com>
2020-04-15 21:33:24 +0800

    Add files via upload

---
+++ js/func_sao/3.js
@@ -0,0 +1,4 @@
+var add = function (w, h) {
+    return w + h;
+};
+console.log(add(1, 2));
---
+++ js/func_sao/3.ts
@@ -0,0 +1,5 @@
+const add = (w:number, h:number) :number => {
+    return w+h;
+}
+
+console.log(add(1,2));
