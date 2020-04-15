675b044e5cc9766b7fdd26ed105a2121a713a5fa
lqt0327 <56208949+lqt0327@users.noreply.github.com>
2020-04-15 21:33:24 +0800

    Add files via upload

---
+++ js/func_sao/2.js
@@ -0,0 +1,6 @@
+// java 强类型语言
+// js 的超集 typescript
+var getArea = function (w, h) {
+    return w * h;
+};
+getArea(111, 222);
---
+++ js/func_sao/2.ts
@@ -0,0 +1,8 @@
+// java 强类型语言
+// js 的超集 typescript
+// tsc 2.ts 运行
+const getArea = (w :number, h :number) :number => {
+    return w * h;
+}
+
+getArea(111, 222)
