e9b20afabae9cfe21202be8ccd04f3ca5e36bc13
lqt0327 <1485215848@qq.com>
2020-07-03 20:13:56 +0800

    classpractice

---
+++ js/reduce/index.js
@@ -0,0 +1,14 @@
+var arr = [1,2,3,4];
+// reduce   total
+// 一句话结果就出来了  arr.map filter ...
+// 1. 可读性 代码风格 
+// 参数
+var sum = arr.reduce(function(pre, cur, index, arr) {
+    console.log(pre,cur,index);
+    return pre + cur;
+},0)
+
+
+console.log(sum);
+    // .map()
+    // .filter() 链式调用 
