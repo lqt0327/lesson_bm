d5c46bedd5dbaf7586d80d88d24c64ed59fd44ce
lqt0327 <1485215848@qq.com>
2020-10-12 18:45:03 +0800

    practice

--- nest/service/src/app.controller.ts
+++ nest/service/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, HttpCode, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// url 的路径配置 
+// http://localhost:3000/app/api/v1
+// http://localhost:3000/app/api/v2
+@Controller('app/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
+  @Post('v3')
+  // @HttpCode(304)
+  @Header('Cache-Control', 'no-cache')
+  @Header('Expires',(new Date(Date.now()+10*1000)).toUTCString())
+  create(): string {
+    return 'This action adds a new cat';
+  }
+  @Get('v1')
+  @Header('Cache-Control', 'no-cache')
+  @Header('ETag',null)
+  @Header('last-modified',(new Date(Date.now()+10*1000)).toUTCString())
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('v2')
+  findAll(): string {
+    return 'This action returns all cats';
   }
 }
