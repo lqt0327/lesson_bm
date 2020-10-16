1cce3204709d35e39c19c36d12af150984afc948
lqt0327 <1485215848@qq.com>
2020-10-16 18:28:48 +0800

    test

--- nest/service/src/app.module.ts
+++ nest/service/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '120.79.55.21',
+      port: 3306,
+      username: 'root',
+      password: 'lqt03271315',
+      database: 'mytest',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
