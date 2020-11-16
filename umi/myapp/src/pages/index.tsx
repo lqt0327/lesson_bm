633273dff8cca3786a880940a3052f920b046a54
lqt0327 <1485215848@qq.com>
2020-11-16 10:13:06 +0800

    practice

--- umi/myapp/src/pages/index.tsx
+++ umi/myapp/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './index.less';
+import { Helmet } from 'umi';
 
 export default () => {
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
+    <div className="application">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>My Title</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
     </div>
   );
 }
